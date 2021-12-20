75441d3381d9ee5b471d914d73370b5eac82c26f
Isayasa663 <isaias.deangeles@iteso.mx>
2021-12-19 21:09:04 -0600

    "Este es mi priner comit chaval"

---
+++ 01/printing-start.py
@@ -0,0 +1,16 @@
+print("hola perrillo, como estas ")
+
+print("hola mundo \nhola mundo \nhola mundo \n")
+
+print("hola sennior" +" Isayas")
+
+print(len(input("Escribe tu nombre chaval-->")))
+
+
+
+city = input("Introduce el nombre de la ciudad donde naciste chaval-->\n")
+
+pet = input("Introduce el nombre de tu primer mascotilla-->\n")
+
+
+print("Tu nombre de banda es-->>> ", city +" "+ pet)
